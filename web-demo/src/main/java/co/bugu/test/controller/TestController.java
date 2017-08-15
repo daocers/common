76fbecfae26533e622dda7e9fecf50a8c977c4be
leiyang <leiyang01@qding.me>
2017-08-15 19:10:39 +0800

    添加web-demo

--- web-demo/src/main/java/co/bugu/test/controller/TestController.java
+++ web-demo/src/main/java/co/bugu/test/controller/TestController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.text.SimpleDateFormat;
+
 /**
  * Created by QDHL on 2017/8/15.
  */
@@ -15,5 +17,7 @@ public class TestController {
     public String test(){
         return "good" + Thread.currentThread();
     }
+
+
 
 }
